25cbec67dd2bf54d56cb94a314b99eb154331de2
srjn45 <srajanpathak45@gmail.com>
2018-04-15 17:45:54 +0530

    build 1.1.1

--- lib/playground/index.ts
+++ lib/playground/index.ts
@@ -6,19 +6,17 @@ import { NgModule } from '@angular/core';
 import { Component } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { SampleModule }  from 'sp-multi-select-dropdown';
-
 @Component({
   selector: 'app',
   template: `<sample-component></sample-component>`
 })
-class AppComponent {}
+class AppComponent { }
 
 @NgModule({
-  bootstrap: [ AppComponent ],
-  declarations: [ AppComponent ],
-  imports: [ BrowserModule, SampleModule ]
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule]
 })
-class AppModule {}
+class AppModule { }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
